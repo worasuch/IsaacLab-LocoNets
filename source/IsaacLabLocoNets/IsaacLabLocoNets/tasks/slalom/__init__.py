0c65e1aa00f817299b1fcac827efc584a68818c0
worasuch <worasuch@gmail.com>
2026-04-16 23:52:34 +0700

    Slalom task

--- source/IsaacLabLocoNets/IsaacLabLocoNets/tasks/slalom/__init__.py
+++ source/IsaacLabLocoNets/IsaacLabLocoNets/tasks/slalom/__init__.py
@@ -15,14 +15,11 @@ from . import agents
 
 
 gym.register(
-    id='default',
+    id='Slalom',
     entry_point=f"{__name__}.slalom_env:SlalomLocomotionTask",
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.slalom_env_cfg:SlalomEnvCfg",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:SlalomPPORunnerCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "es_cfg_entry_point": f"{agents.__name__}:es_cfg.yaml",
     },
 )
